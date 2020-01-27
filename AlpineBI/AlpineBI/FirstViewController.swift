afb7f927430e518a20fac79c3b205c995cc93a86
munishsethi777 <munishsethi777@gmail.com>
2020-01-27 11:34:28 +0530

    first commit

---
+++ AlpineBI/AlpineBI/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AlpineBI
+//
+//  Created by Munish Sethi on 23/01/20.
+//  Copyright © 2020 Munish Sethi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AlpineBI/AlpineBI/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AlpineBI
+//
+//  Created by Munish Sethi on 23/01/20.
+//  Copyright © 2020 Munish Sethi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
