dd51ff027465496afbeea697a401254fd22c77b9
munishsethi777 <munishsethi777@gmail.com>
2020-02-01 12:15:34 +0530

    loginview background gradient

--- AlpineBI/AlpineBI/LoginViewController.swift
+++ AlpineBI/AlpineBI/LoginViewController.swift
@@ -23,6 +23,7 @@ class LoginViewController : UIViewController{
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        setGradientBackground()
         userNameText.text = ""
         passwordTextfield.text = ""
     }
@@ -83,5 +84,17 @@ class LoginViewController : UIViewController{
                 return
         }
         loginButton.isEnabled = true
+    }
+    
+    func setGradientBackground() {
+        let colorBottom =  UIColor(red: 0.0/255.0, green: 123.0/255.0, blue: 127.0/255.0, alpha: 1.0).cgColor
+        let colorTop = UIColor(red: 0.0/255.0, green: 229.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        self.view.layer.insertSublayer(gradientLayer, at:0)
     }
 }
