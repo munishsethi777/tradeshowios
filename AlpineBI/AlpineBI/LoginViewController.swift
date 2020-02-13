4959995bf83838a526b9eda76af7e4d7e92c8da6
munishsethi777 <munishsethi777@gmail.com>
2020-02-13 13:50:01 +0530

    keyboard login height adjusted

--- AlpineBI/AlpineBI/LoginViewController.swift
+++ AlpineBI/AlpineBI/LoginViewController.swift
@@ -40,7 +40,7 @@ class LoginViewController : UIViewController{
     @objc func keyboardWasShown(notification: NSNotification){
         var info = notification.userInfo!
         let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size
-        let contentInsets : UIEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize!.height + 60, right: 0.0)
+        let contentInsets : UIEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize!.height + 85, right: 0.0)
         self.scrollView.contentInset = contentInsets
         self.scrollView.scrollIndicatorInsets = contentInsets
     }
