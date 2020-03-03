f28b826e36662cea3fddc3165a3215643d59cfa0
bgaheer <baljeetgaheer@gmail.com>
2020-03-03 12:00:53 +0530

    Show Saved Spring Detail

---
+++ AlpineBI/AlpineBI/ShowSpringQestionsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ShowSpringQestionsViewController.swift
+//  AlpineBI
+//
+//  Created by Baljeet Gaheer on 02/03/20.
+//  Copyright © 2020 Munish Sethi. All rights reserved.
+//
+
+import Foundation
---
+++ AlpineBI/AlpineBI/ShowSpringQuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShowSpringQuestionTableViewCell.swift
+//  AlpineBI
+//
+//  Created by Baljeet Gaheer on 02/03/20.
+//  Copyright © 2020 Munish Sethi. All rights reserved.
+//
+
+import UIKit
+
+class ShowSpringQuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
