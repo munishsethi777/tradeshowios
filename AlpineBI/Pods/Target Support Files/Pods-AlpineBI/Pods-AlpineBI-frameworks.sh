038cde3fa67db07480ab3c0979a79ade0c5a0eff
bgaheer <baljeetgaheer@gmail.com>
2020-03-04 15:09:32 +0530

    TOPCropViewController Pod install

--- AlpineBI/Pods/Target Support Files/Pods-AlpineBI/Pods-AlpineBI-frameworks.sh
+++ AlpineBI/Pods/Target Support Files/Pods-AlpineBI/Pods-AlpineBI-frameworks.sh
@@ -161,9 +161,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CropViewController/CropViewController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RSSelectionMenu/RSSelectionMenu.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CropViewController/CropViewController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RSSelectionMenu/RSSelectionMenu.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
