684eb80760a3ceacc99ebfbfe284c38ff0212d02
bgaheer <baljeetgaheer@gmail.com>
2020-03-14 11:59:51 +0530

    Splash Screen Changes

--- AlpineBI/AlpineBI/SplashScreenViewController.swift
+++ AlpineBI/AlpineBI/SplashScreenViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class SplashScreenViewController: UIViewController {
-
+    var gradientLayer: CAGradientLayer!
+    
     override func viewDidLoad() {
+        setGradientBackground()
         super.viewDidLoad()
     }
+    
+    @IBOutlet weak var labelview: UILabel!
+    
     override func viewDidAppear(_ animated: Bool) {
         let loggedInUserSeq = PreferencesUtil.sharedInstance.getLoggedInUserSeq()
         UIApplication.shared.applicationIconBadgeNumber = 0
@@ -21,5 +26,20 @@ class SplashScreenViewController: UIViewController {
         }else{
             self.performSegue(withIdentifier: "showLoginViewController", sender: nil)
         }
+    }
+    
+    func setGradientBackground() {
+        let colorBottom =  UIColor(red: 0.0/255.0, green: 123.0/255.0, blue: 127.0/255.0, alpha: 1.0).cgColor
+        let colorTop = UIColor(red: 0.0/255.0, green: 229.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+        gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        gradientLayer.frame = self.view.bounds
     }
 }
